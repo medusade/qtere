2202723a4b72f347501efca4cc18dcab09cc9c87
medusade <medusade@cox.net>
2016-09-13 10:25:05 -0700

    Updated source.

--- src/Qtere/App/Gui/Hello/MainWindow.cpp
+++ src/Qtere/App/Gui/Hello/MainWindow.cpp
@@ -59,11 +59,26 @@ bool MainWindow::beforeShow() {
 
         LOG_DEBUG("this->setCentralWidget(m_byeButton)...");
         this->setCentralWidget(m_byeButton);
+
+        LOG_DEBUG("connect (&m_thread, SIGNAL(tick()), this, SLOT(onTick()))...");
+        connect (&m_thread, SIGNAL(tick()), this, SLOT(onTick()));
+
+        LOG_DEBUG("m_thread.start()...");
+        m_thread.start();
     }
     return true;
 }
 bool MainWindow::beforeDestroy() {
     if ((m_byeButton)) {
+        LOG_DEBUG("enit m_thread.done()...");
+        emit m_thread.done();
+
+        LOG_DEBUG("m_thread.wait()...");
+        m_thread.wait();
+
+        LOG_DEBUG("disconnect (&m_thread, SIGNAL(tick()), this, SLOT(onTick()))...");
+        disconnect (&m_thread, SIGNAL(tick()), this, SLOT(onTick()));
+
         LOG_DEBUG("disconnect (this, SIGNAL(onBye()), &m_mainSlots, SLOT(onBye()))...");
         disconnect (this, SIGNAL(onBye()), &m_mainSlots, SLOT(onBye()));
 
@@ -85,6 +100,14 @@ void MainWindow::byeReleased() {
     LOG_DEBUG("emit onBye()...");
     emit onBye();
 }
+void MainWindow::onTick() {
+    unsigned long sleepSeconds = 1;
+    LOG_DEBUG("sleepSeconds = ((emit m_thread.sleepSeconds()) + 1)...");
+    sleepSeconds = ((emit m_thread.sleepSeconds()) + 1);
+
+    LOG_DEBUG("emit m_thread.setSleepSeconds( " << sleepSeconds << ")...");
+    emit m_thread.setSleepSeconds(sleepSeconds);
+}
 
 ///////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
@@ -93,11 +116,66 @@ void MainWindow::closeEvent(QCloseEvent *event) {
     event->accept();
 }
 
+///////////////////////////////////////////////////////////////////////
+///  Class: Thread
+///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+Thread::Thread(unsigned long sleepSeconds)
+: m_sleepSeconds(sleepSeconds), m_done(false) {
+}
+Thread::~Thread() {
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+void Thread::run() {
+    LOG_DEBUG("connect (this, SIGNAL(done()), this, SLOT(onDone()))...");
+    connect (this, SIGNAL(done()), this, SLOT(onDone()));
+
+    LOG_DEBUG("connect (this, SIGNAL(setSleepSeconds(unsigned long)), this, SLOT(onSetSleepSeconds(unsigned long)))...");
+    connect (this, SIGNAL(setSleepSeconds(unsigned long)), this, SLOT(onSetSleepSeconds(unsigned long)));
+
+    LOG_DEBUG("connect (this, SIGNAL(sleepSeconds()), this, SLOT(onSleepSeconds()))...");
+    connect (this, SIGNAL(sleepSeconds()), this, SLOT(onSleepSeconds()));
+
+    LOG_DEBUG("for (m_done = false; !m_done; )...");
+    for (m_done = false; !m_done; ) {
+        int sleepMilliSeconds = m_sleepSeconds*1000;
+
+        LOG_DEBUG("m_sem.tryAcquire(1, sleepMilliSeconds = " << sleepMilliSeconds << ")...");
+        m_sem.tryAcquire(1, sleepMilliSeconds);
+
+        LOG_DEBUG("emit tick()...");
+        emit tick();
+    }
+    LOG_DEBUG("disconnect (this, SIGNAL(sleepSeconds()), this, SLOT(onSleepSeconds()))...");
+    disconnect (this, SIGNAL(sleepSeconds()), this, SLOT(onSleepSeconds()));
+
+    LOG_DEBUG("disconnect (this, SIGNAL(setSleepSeconds(unsigned long)), this, SLOT(onSetSleepSeconds(unsigned long)))...");
+    disconnect (this, SIGNAL(setSleepSeconds(unsigned long)), this, SLOT(onSetSleepSeconds(unsigned long)));
+
+    LOG_DEBUG("disconnect (this, SIGNAL(done()), this, SLOT(onDone()))...");
+    disconnect (this, SIGNAL(done()), this, SLOT(onDone()));
+}
+void Thread::onDone() {
+    LOG_DEBUG("m_done = true...");
+    m_done = true;
+
+    LOG_DEBUG("m_sem.release(1)...");
+    m_sem.release(1);
+}
+void Thread::onSetSleepSeconds(unsigned long to) {
+    LOG_DEBUG("m_sleepSeconds = " << to << "...");
+    m_sleepSeconds = to;
+}
+unsigned long Thread::onSleepSeconds() {
+    LOG_DEBUG("return " << m_sleepSeconds << "...");
+    return m_sleepSeconds;
+}
+
 } // namespace Hello
 } // namespace Gui 
 } // namespace App 
 } // namespace Qtere 
-
-
-        
-
